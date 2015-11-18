543e0602342d9ff5aa4505ee382b36af5ec71f8c
Brandon Blair <brbun1@hotmail.com>
2015-11-17 20:32:13 -0800

    initial commit

---
+++ Kickball/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kickball.Startup))]
+namespace Kickball
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
