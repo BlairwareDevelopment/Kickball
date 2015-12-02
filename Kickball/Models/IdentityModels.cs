10152ff55506e873ed9029e1a7c9447040d92e23
Brandon Blair <brbun1@hotmail.com>
2015-12-01 21:07:57 -0800

    add azure connection string

--- Kickball/Models/IdentityModels.cs
+++ Kickball/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Kickball.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AzureConnection", throwIfV1Schema: false)
         {
         }
 
